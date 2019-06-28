89e49d4cdd79864de5c0342f4501d1b9b79b1e41
Dmitry Petukhov <dp@simplexum.com>
2019-06-28 18:04:10 +0500

    make BIP70 example functional
    Not sure if this is still relevant, but if we have examples,
    they should be functional

--- examples/bip-0070-payment-protocol.py
+++ examples/bip-0070-payment-protocol.py
@@ -17,25 +17,34 @@ Creates http response objects suitable for use with
 bitcoin bip 70 using googles protocol buffers.
 """
 
-import urllib2
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+# generate paymentrequest_pb2 with protobuf compiler as described in
+# https://developers.google.com/protocol-buffers/docs/pythontutorial
+# using .proto file located at
 # https://github.com/bitcoin/bips/blob/master/bip-0070/paymentrequest.proto
-import payments_pb2
-o = payments_pb2
+import paymentrequest_pb2 as o
 
-import bitcointx
-#bitcointx.select_chain_params('bitcoin/testnet')
-from bitcointx.wallet import CBitcoinAddress
+# import bitcointx
+
+from bitcointx.wallet import CCoinAddress
 from bitcointx.core.script import CScript
-from bitcointx.rpc import Proxy
+from bitcointx.rpc import RPCCaller
 
 from time import time
+
+# bitcointx.select_chain_params('bitcoin/regtest')
+
+listen_on = {'host': '127.0.0.1', 'port': 8080}
+req_url_path = 'payment_request'
+ack_url_path = 'payment_ack'
+
 
 def payment_request():
     """Generates a http PaymentRequest object"""
 
-    bc = Proxy()
-    btc = bc.getnewaddress()
+    bc = RPCCaller()
+    btc = CCoinAddress(bc.getnewaddress())
 
 #   Setting the 'amount' field to 0 (zero) should prompt the user to enter
 #   the amount for us but a bug in bitcoin core qt version 0.9.1 (at time of
@@ -45,39 +54,72 @@ def payment_request():
     btc_amount = 100000
     serialized_pubkey = btc.to_scriptPubKey()
 
-    pdo = o.PaymentDetails()
-    #pdo.network = 'test'
-    pdo.outputs.add(amount = btc_amount,script = serialized_pubkey)
+    pdo = o.PaymentDetails(network="regtest")
+    # pdo.network = 'test'
+    pdo.outputs.add(amount=btc_amount, script=serialized_pubkey)
     pdo.time = int(time())
     pdo.memo = 'String shown to user before confirming payment'
-    pdo.payment_url = 'http://payment_ack.url'
+    pdo.payment_url = 'http://{}:{}/{}'.format(listen_on['host'],
+                                               listen_on['port'],
+                                               ack_url_path)
 
     pro = o.PaymentRequest()
     pro.serialized_payment_details = pdo.SerializeToString()
 
     sds_pr = pro.SerializeToString()
 
-    open('sds_pr_blob', 'wb').write(sds_pr)
     headers = {'Content-Type': 'application/bitcoin-payment',
                'Accept': 'application/bitcoin-paymentrequest'}
-    http_response_object = urllib2.Request('file:sds_pr_blob', None, headers)
 
-    return http_response_object
+    return sds_pr, headers
 
 
-def payment_ack(serialized_Payment_message):
-    """Generates a PaymentACK object, captures client refund address and returns a message"""
+def payment_ack(serialized_payment_message):
+    """Generates a PaymentACK object, captures client refund address
+    and returns a tuple (message, refund_address)"""
 
     pao = o.PaymentACK()
-    pao.payment.ParseFromString(serialized_Payment_message)
+    pao.payment.ParseFromString(serialized_payment_message)
     pao.memo = 'String shown to user after payment confirmation'
 
-    refund_address = CBitcoinAddress.from_scriptPubKey(CScript(pao.payment.refund_to[0].script))
+    refund_address = CCoinAddress.from_scriptPubKey(
+        CScript(pao.payment.refund_to[0].script))
 
     sds_pa = pao.SerializeToString()
 
-    open('sds_pa_blob', 'wb').write(sds_pa)
-    headers = {'Content-Type' : 'application/bitcoin-payment', 'Accept' : 'application/bitcoin-paymentack'}
-    http_response_object = urllib2.Request('file:sds_pa_blob', None, headers)
+    headers = {'Content-Type': 'application/bitcoin-payment',
+               'Accept': 'application/bitcoin-paymentack'}
 
-    return http_response_object
+    return sds_pa, headers, refund_address
+
+
+class ReqHandler(BaseHTTPRequestHandler):
+
+    def _common_resp(self, data, headers):
+        self.send_response(200)
+        for k, v in headers.items():
+            self.send_header(k, v)
+        self.end_headers()
+        self.wfile.write(data)
+
+    def do_GET(self):
+        if self.path == '/' + req_url_path:
+            data, headers = payment_request()
+            self._common_resp(data, headers)
+        else:
+            self.send_error(404, 'wrong url')
+
+    def do_POST(self):
+        if self.path == '/' + ack_url_path:
+            content_length = int(self.headers['Content-Length'])
+            payment_message = self.rfile.read(content_length)
+            data, headers, refund_address = payment_ack(payment_message)
+            print("Client's refund address: ", str(refund_address))
+            self._common_resp(data, headers)
+        else:
+            self.send_error(404, 'wrong url')
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer((listen_on['host'], listen_on['port']), ReqHandler)
+    httpd.serve_forever()
